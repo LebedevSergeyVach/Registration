f691f8d1dd54238ed99ef5dfbc340583db129a19
LebedevSergeyV <ob430y@yandex.ru>
2023-11-30 01:21:59 +0700

    Update MainActivity & UserRegistration

--- app/src/main/java/com/example/registration/MainActivity.java
+++ app/src/main/java/com/example/registration/MainActivity.java
@@ -8,43 +8,74 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int REGISTRATION_REQUEST_CODE = 1;
     private String LOGIN = "admin";
     private String PASSWORD = "password";
+
+    private Button loginButton;
+
+    private String login;
+    private String password;
+
+    private String right;
+    private String login_error;
+    private TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button loginButton = findViewById(R.id.loginButton);
+        loginButton = findViewById(R.id.loginButton);
 
         loginButton.setOnClickListener(view -> {
+            login = ((EditText) findViewById(R.id.login)).getText().toString();
+            password = ((EditText) findViewById(R.id.password)).getText().toString();
 
-            String login = ((EditText) findViewById(R.id.login)).getText().toString();
-            String password = ((EditText) findViewById(R.id.password)).getText().toString();
-
-            TextView result = findViewById(R.id.answer);
+            result = findViewById(R.id.answer);
 
             if (login.equals(LOGIN) && password.equals(PASSWORD)) {
-                String right = getString(R.string.right);
+                right = getString(R.string.right);
                 result.setText(right);
                 result.setTextColor(Color.GREEN);
 
                 startActivity(new Intent(MainActivity.this, Welcome.class));
             } else {
-                String login_error = getString(R.string.error_login);
+                startRegistrationActivity();
 
+                login_error = getString(R.string.error_login);
                 result.setText(login_error);
                 result.setTextColor(Color.RED);
 
                 ((EditText) findViewById(R.id.login)).setText("");
                 ((EditText) findViewById(R.id.password)).setText("");
-
-                startActivity(new Intent(MainActivity.this, UserRegistration.class));
             }
         });
+    }
+
+    private void startRegistrationActivity() {
+        Intent intent = new Intent(MainActivity.this, UserRegistration.class);
+        startActivityForResult(intent, REGISTRATION_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REGISTRATION_REQUEST_CODE && resultCode == RESULT_OK) {
+
+            String registeredLogin = data.getStringExtra("login");
+            String registeredPassword = data.getStringExtra("login");
+
+            ((EditText) findViewById(R.id.login)).setText(registeredLogin);
+            ((EditText) findViewById(R.id.password)).setText(registeredPassword);
+
+            result = findViewById(R.id.answer);
+            right = getString(R.string.answer_entrance);
+            result.setText(right);
+            result.setTextColor(Color.BLUE);
+        }
     }
 }
--- app/src/main/java/com/example/registration/UserRegistration.java
+++ app/src/main/java/com/example/registration/UserRegistration.java
@@ -1,28 +1,35 @@
 package com.example.registration;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.graphics.Color;
+
 import android.os.Bundle;
+
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class UserRegistration extends AppCompatActivity {
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.user_registration);
 
-        Button loginButton = findViewById(R.id.loginButton);
+        Button registrationButton = findViewById(R.id.registrationButton);
 
-        loginButton.setOnClickListener(view -> {
+        registrationButton.setOnClickListener(view -> {
 
             String userLogin = ((EditText) findViewById(R.id.user_login)).getText().toString();
             String userPassword = ((EditText) findViewById(R.id.user_password)).getText().toString();
 
-            startActivity(new Intent(UserRegistration.this, MainActivity.class));
+            Intent resultIntent = new Intent(UserRegistration.this, MainActivity.class);
+            resultIntent.putExtra("login", userLogin);
+            resultIntent.putExtra("password", userPassword);
+            setResult(RESULT_OK, resultIntent);
+            finish();
         });
     }
 }
